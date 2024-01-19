0e7ee5bd625ed476d5d887e22fc4c9a15b0414cc
diegogirao <diegoramiro25@gmail.com>
2024-01-19 20:10:57 +0000

    feat: create form component

--- app/components/Form.tsx
+++ app/components/Form.tsx
@@ -1,4 +1,71 @@
+import { useForm } from "react-hook-form"
+import { FormData } from "@/types"
+import FormField from "./FormField"
+
 function Form() {
-	return <div>Form</div>
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+		setError,
+	} = useForm<FormData>()
+
+	const onSubmit = async (data: FormData) => {
+		console.log("SUCCESS", data)
+	}
+
+	return (
+		<form onSubmit={handleSubmit(onSubmit)}>
+			<div className="grid col-auto">
+				<h1 className="text-3xl font-bold mb-4">Zod 'N React-Hook-Form</h1>
+				<FormField
+					type="email"
+					placeholder="johndoe@mail.com"
+					name="email"
+					register={register}
+					error={errors.email}
+				/>
+				<FormField
+					type="text"
+					placeholder="https://github.com/john-doe"
+					name="githubUrl"
+					register={register}
+					error={errors.githubUrl}
+				/>
+				<FormField
+					type="text"
+					placeholder="https://www.linkedin.com/in/john-doe"
+					name="linkedinUrl"
+					register={register}
+					error={errors.linkedinUrl}
+				/>
+				<FormField
+					type="number"
+					placeholder="Years of Experience (1 - 10)"
+					name="yearsOfExperience"
+					register={register}
+					error={errors.yearsOfExperience}
+					valueAsNumber
+				/>
+				<FormField
+					type="password"
+					placeholder="Password"
+					name="password"
+					register={register}
+					error={errors.password}
+				/>
+				<FormField
+					type="password"
+					placeholder="Confirm Password"
+					name="confirmPassword"
+					register={register}
+					error={errors.confirmPassword}
+				/>
+				<button type="submit" className="submit-button">
+					Submit
+				</button>
+			</div>
+		</form>
+	)
 }
 export default Form
